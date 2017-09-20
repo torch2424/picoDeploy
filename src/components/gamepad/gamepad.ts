a7ba5aabb5bde02d44e6d959c9ef5f2c561f25a7
Aaron Turner <aaron@aaronthedev.com>
2017-09-20 02:55:36 -0700

    Got a good basis for how we are going to develop gamepad

--- src/components/gamepad/gamepad.ts
+++ src/components/gamepad/gamepad.ts
@@ -12,8 +12,41 @@ import { Component } from '@angular/core';
 })
 export class GamepadComponent {
 
+  buttons: any
+
   constructor() {
     console.log('Hello GamepadComponent Component');
   }
+
+  ngOnInit() {
+
+    // Get our buttons
+    this.buttons = {
+      dpad: document.getElementById('dpad'),
+      squareBtn: document.getElementById('squareBtn'),
+      crossBtn: document.getElementById('squareBtn')
+    }
+
+    // Loop through our buttons and add touch events
+    // use element.getBoundingRect() top, bottom, left, right to get clientX and clientY in touch events :)
+    // https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+    Object.keys(this.buttons).forEach((button) => {
+      this.buttons[button].addEventListener("touchstart", function (event) {
+        Array.from(event.touches).forEach(touch => {
+          console.log('Hai!', touch);
+        })
+        //pico8_buttons_event(event, 0);
+      }, false);
+      this.buttons[button].addEventListener("touchmove", function (event) {
+        //pico8_buttons_event(event, 1);
+      }, false);
+      this.buttons[button].addEventListener("touchend", function (event) {
+        //pico8_buttons_event(event, 2);
+      }, false);
+    });
+  }
+
+  // Our handler function for touch events
+
 
 }
