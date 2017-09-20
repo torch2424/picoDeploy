99a7dc626e0e5c2182bb2dc88e30acc6914f6e7c
Aaron Turner <aaron@aaronthedev.com>
2017-09-20 15:19:01 -0700

    FINISHED MOBILE CONTROLS!!!!1!

--- src/components/gamepad/gamepad.ts
+++ src/components/gamepad/gamepad.ts
@@ -20,19 +20,24 @@ export class GamepadComponent {
 
   ngOnInit() {
 
-    // Pico 8 does some craziness where the is a buttons object on the window with an array of all buttons that need to be toggles.
-    (<any>window).pico8_buttons = [0,0,0,0,0,0,0,0];
-
     // Get our buttons, and their position
     this.buttons = {
       dpad: {
-        element: document.getElementById('dpad')
+        element: document.getElementById('dpad'),
+        pressed: {
+          left: false,
+          right: false,
+          up: false,
+          down: false
+        }
       },
       squareBtn: {
-        element: document.getElementById('squareBtn')
+        element: document.getElementById('squareBtn'),
+        pressed: false
       },
       crossBtn: {
-        element: document.getElementById('crossBtn')
+        element: document.getElementById('crossBtn'),
+        pressed: false
       }
     }
 
@@ -41,10 +46,8 @@ export class GamepadComponent {
     // https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
     Object.keys(this.buttons).forEach((button) => {
       const buttonBoundingRect = this.buttons[button].element.getBoundingClientRect();
-      this.buttons[button] = Object.assign(this.buttons[button], buttonBoundingRect);
+      this.buttons[button].rect = buttonBoundingRect;
     });
-
-
 
     // Loop through our buttons and add touch events
     Object.keys(this.buttons).forEach((button) => {
@@ -58,6 +61,9 @@ export class GamepadComponent {
         this.touchEventHandler(event);
       }, false);
     });
+
+    // Poll to keep updating the controller state
+    window.requestAnimationFrame(() => this.updatePico8Controller());
   }
 
   // Our handler function for touch events
@@ -70,36 +76,108 @@ export class GamepadComponent {
       console.log('Hai!', touch);
     });
 
-    // Get our keycode
-    let keyCode = 0;
-
-    // Handle Dpad events
-    if(event.target.id === 'dpad') {
-      keyCode = 39
-    }
-
-    // Handle Square button
-    if(event.target.id === 'squareBtn') {
-      keyCode = 90
-    }
-
-    // Handle Cross Button
-    if(event.target.id === 'crossBtn') {
-      keyCode = 88
-    }
-
-
     // Get our key event info
     if(event.type === "touchstart" || event.type === "touchmove") {
-      //this.simulateKeyEvent('press', keyCode);
-      //this.simulateKeyEvent('down', keyCode);
-      //pico8_buttons = [  ]
+
+      // Handle Dpad events
+      if(event.target.id === 'dpad') {
+        // Reset the dpad, only one direction at a time
+        this.resetDpad();
+
+        // Calculate for the correct key
+        // Only using the first touch, since we shouldn't be having two fingers on the dpad
+        const touch = event.touches[0];
+
+        // Find if the horizontal or vertical influence is greater
+        const isHorizontal =
+          Math.abs(this.buttons.dpad.rect.left + (this.buttons.dpad.rect.width / 2) - touch.clientX) >
+          Math.abs(this.buttons.dpad.rect.top + (this.buttons.dpad.rect.height / 2) - touch.clientY);
+
+        // Find the greatest absolute value, -X, +X, -Y, +Y
+        if(isHorizontal) {
+          const isLeft = this.buttons.dpad.rect.left + (this.buttons.dpad.rect.width / 2) > touch.clientX;
+          if(isLeft) {
+            this.buttons.dpad.pressed.left = true;
+          } else {
+            this.buttons.dpad.pressed.right = true;
+          }
+        } else {
+          const isUp = this.buttons.dpad.rect.top + (this.buttons.dpad.rect.height / 2) > touch.clientY;
+          if(isUp) {
+            this.buttons.dpad.pressed.up = true;
+          } else {
+            this.buttons.dpad.pressed.down = true;
+          }
+        }
+      }
+
+      // Handle Square button
+      if(event.target.id === 'squareBtn') {
+        this.buttons.squareBtn.pressed = true;
+      }
+
+      // Handle Cross Button
+      if(event.target.id === 'crossBtn') {
+        this.buttons.squareBtn.pressed = true;
+      }
     } else {
-      //this.simulateKeyEvent('up', keyCode);
+
+      // Handle Dpad events
+      if(event.target.id === 'dpad') {
+        this.resetDpad();
+      }
+
+      // Handle Square button
+      if(event.target.id === 'squareBtn') {
+        this.buttons.squareBtn.pressed = false;
+      }
+
+      // Handle Cross Button
+      if(event.target.id === 'crossBtn') {
+        this.buttons.squareBtn.pressed = false;
+      }
     }
   }
 
-  // Our Pico 8 Gamepad from: https://github.com/krajzeg/pico8gamepad
+  resetDpad() {
+    if(!this.buttons || !this.buttons.dpad || !this.buttons.dpad.pressed) {
+      return;
+    }
 
+    Object.keys(this.buttons.dpad.pressed).forEach(dpadKey => {
+      this.buttons.dpad.pressed[dpadKey] = false;
+    });
+  }
+
+  // Our Pico 8 Gamepad with help from: https://github.com/krajzeg/pico8gamepad
+  updatePico8Controller() {
+    let bitmask = 0;
+
+    // Go through all of our buttons for the bitmask
+    if(this.buttons.dpad.pressed.left) {
+      bitmask |= 1;
+    }
+    if(this.buttons.dpad.pressed.right) {
+      bitmask |= 2;
+    }
+    if(this.buttons.dpad.pressed.up) {
+      bitmask |= 4;
+    }
+    if(this.buttons.dpad.pressed.down) {
+      bitmask |= 8;
+    }
+    if(this.buttons.squareBtn.pressed) {
+      bitmask |= 16;
+    }
+    if(this.buttons.crossBtn.pressed) {
+      bitmask |= 32;
+    }
+
+    // Set for player one
+    (<any>window).pico8_buttons[0] |= bitmask;
+
+    // Call the update next frame
+    window.requestAnimationFrame(() => this.updatePico8Controller());
+  }
 
 }
