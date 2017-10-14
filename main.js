45b262ea88f5c2d4bcf8dc197e788f0248533b60
Aaron Turner <aaron@aaronthedev.com>
2017-10-13 19:05:28 -0700

    Fixed up electron building

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "picoDeploy",
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +22,9 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  if(process.env.DEV) {
+    win.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
   win.on('closed', () => {
